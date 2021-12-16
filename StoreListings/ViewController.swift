2547f1fd5f45f647bdaeeb53d3dc3fa545687cae
Kelvin Ching Kiu Chan <chankelvin74@gmail.com>
2021-12-15 21:32:02 -0800

    Initial Commit

---
+++ StoreListings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreListings
+//
+//  Created by Kelvin CK Chan on 12/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
